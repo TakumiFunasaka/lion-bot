b4a555267da5634198b43d45e30a00f1c4c2ef2e
mmmatch <playstar.vista@gmail.com>
2022-04-17 07:15:39 +0900

    初期処理など

--- functions/slackConnector.js
+++ functions/slackConnector.js
@@ -1,10 +1,46 @@
 const functions = require('firebase-functions');
-exports.slackConnector = functions.https.onRequest((req, res) => {
-  // ...
-  const payload = req.body;
-  if (payload.type === 'url_verification') {
-      return res.status(200).json({ 'challenge': payload.challenge });
-  }  
-});
+const {verifyRequestSignature} = require('@slack/events-api');
+
+const verifyWebhook = req => {
+  const signature = {
+    signingSecret: process.env.SLACK_SECRET,
+    requestSignature: req.headers['x-slack-signature'],
+    requestTimestamp: req.headers['x-slack-request-timestamp'],
+    body: req.rawBody,
+  };
+  verifyRequestSignature(signature);
+};
+
+
+exports.slackConnector =  async (req, res) => {
+  try {
+    if (req.method !== 'POST') {
+      const error = new Error('Only POST requests are accepted');
+      error.code = 405;
+      throw error;
+    }
+    // Slackからの認証かどうか
+    verifyWebhook(req);
+    // 初期認証対応
+    const payload = req.body
+    if(payload.type === 'url_verification'){
+      return res.status(200).json({'challenge': payload.challenge});
+    }
+
+    // kibela APIにメッセージを投げる
+
+    // 画像生成
+
+    // slackに投稿
+
+    return Promise.resolve();
+
+  } catch (err) {
+    console.error(err);
+    res.status(err.code || 500).send(err);
+    return Promise.reject(err);
+  }
+
+};
 
 
