5dbf7db05435bae918e62185ca56a18d8c33619d
TakumiFunasaka <takumi.funasaka@gmail.com>
2022-04-17 01:16:08 +0900

    add generate function

--- functions/imageGenerator.js
+++ functions/imageGenerator.js
@@ -1,96 +1,113 @@
-// const fs = require("fs");
-// const functions = require("firebase-functions");
-// const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
+const functions = require("firebase-functions");
+const { createCanvas } = require("canvas");
+const base64 = require("urlsafe-base64");
 
-// const dummyData = {
-//   data: {
-//     note: {
-//       title: "利用規約・プライバシーポリシーページの更新について",
-//       author: {
-//         id: "VXNlci8xNjg",
-//         realName: "Muneaki Hayakawa",
-//         avatarImage: {
-//           url: "https://cdn.kibe.la/media/private/7504/W1siZiIsInRlYW1fNzUwNC8yMDE4LzExLzIwLzhnNWVyNmoyaGpfNjFHcnRNQTNVc0wuX1NZNDUwXy5qcGciXSxbInAiLCJhdXRvX29yaWVudCJdLFsicCIsInRodW1iIiwiNDB4NDAjIl1d/8826578b26fe1b2c/61GrtMA3UsL._SY450_.jpg",
-//         },
-//       },
-//     },
-//   },
-// };
+// canvasの横幅
+let canvasWidth = 1200;
+// canvasの縦幅
+let canvasHeight = 630;
+let canvas;
+let ctx;
 
-// exports.ogp = async (req, res) => {
-//   const { text } = req.query;
-//   const url = await createOgp(text);
-//   res.redirect(url);
-// };
 
-// const fontStyle = {
-//   font: 'bold 85px "Noto Sans CJK JP"',
-//   lineHeight: 100,
-//   color: "#FFFFFF",
-// };
+const dummyData = {
+  data: {
+    note: {
+      title: "利用規約・プライバシーポリシーページの更新について",
+    },
+  },
+};
 
-// const createOgp = async (text) => {
-//   const loaclTargetPath = `/tmp/target.png`;
-//   const localBasePath = "/tmp/base.png";
-//   const targetPath = `ogps/${text}.png`;
-//   const basePath = "ogps/base.png";
 
-//   // 背景画像のダウンロード
-//   await bucket.file(basePath).download({ destination: localBasePath });
+// タイトル部分の文字スタイル
+const titleFontStyle = {
+  font: 'bold 73px "Noto Sans CJK JP"',
+  lineHeight: 80,
+  color: '#FFFFFF'
+};
+// 本文部分の文字スタイル
+const bodyFontStyle = {
+  font: '30px "Noto Sans CJK JP"',
+  lineHeight: 38,
+  color: '#333333'
+};
+// 画像内側余白
+let padding = 80;
 
-//   const canvas = createCanvas(1280, 670);
-//   const ctx = canvas.getContext("2d");
-//   // 背景画像の描画
-//   const baseImage = await loadImage(localBasePath);
-//   ctx.drawImage(baseImage, 0, 0, 1280, 670);
+// 背景色
+let backgroundColor = "#327AC2";
 
-//   // 文字列の書き込み
-//   ctx.font = fontStyle.font;
-//   ctx.fillStyle = fontStyle.color;
-//   ctx.textBaseline = "top";
-//   const topPadding = 60;
-//   const leftPadding = 50;
-//   const rightPadding = 600;
-//   const lineWidth = 1280 - leftPadding - rightPadding;
-//   const lines = splitByMeasureWidth(name, lineWidth, ctx);
-//   const lineCount = lines.length;
+exports.generate = functions.https.onRequest((request, response) =>  {
+  const title = dummyData.data.note.title;
+  const fileName = "test.png";
 
-//   for (let index = 0; index < lineCount; index++) {
-//     const element = lines[index];
-//     ctx.fillText(
-//       element,
-//       leftPadding,
-//       topPadding + fontStyle.lineHeight * index
-//     );
-//   }
+  // 空白のcanvasを作成
+  canvas = createCanvas(canvasWidth, canvasHeight)
+  // コンテキスト取得
+  ctx = canvas.getContext('2d')
+  // -----
+  // タイトル描画
+  // -----
+  // 行長さ
+  let lineWidth = canvasWidth - (padding * 2);
+  // フォント設定
+  ctx.font = titleFontStyle.font;
+  // 行数の割り出し
+  let titleLines = splitByMeasureWidth(title, lineWidth, ctx);
+  let titleLineCnt = titleLines.length;
+  // タイトル分の高さ
+  let titleHeight = titleLines.length * titleFontStyle.lineHeight;
 
-//   // tmpディレクトリへの書き込み
-//   const buf = canvas.toBuffer();
-//   fs.writeFileSync(loaclTargetPath, buf);
+  // 行高さと余白が最小高さ(630)を上回る場合はカンバスをリサイズする
+  let contentHeight = titleHeight + (padding * 2);
+  if (canvasHeight < contentHeight) {
+      canvasHeight = contentHeight
+      canvas = createCanvas(canvasWidth, contentHeight)
+      ctx = canvas.getContext('2d')
+  }
 
-//   // Storageにアップロード
-//   await bucket.upload(loaclTargetPath, { destination: targetPath });
+  // 取り敢えず背景色をつける
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight)
 
-//   // tmpファイルの削除
-//   fs.unlinkSync(localBasePath);
-//   fs.unlinkSync(loaclTargetPath);
+  // 文字描画のベースラインを設定
+  ctx.textBaseline = 'top';
+  // タイトルを描画
+  ctx.fillStyle = titleFontStyle.color;
+  ctx.font = titleFontStyle.font;
+  for (let index = 0; index < titleLineCnt; index++) {
+      const element = titleLines[index];
+      ctx.fillText(element, padding, padding + (titleFontStyle.lineHeight * index))
+  }
 
-//   return getUrl(targetPath);
-// };
+  let b64 = canvas.toDataURL().split(',');
+  let img = base64.decode(b64[1]);
 
-// const splitByMeasureWidth = (str, maxWidth, context) => {
-//   // サロゲートペアを考慮した文字分割
-//   const chars = Array.from(str);
-//   let line = "";
-//   const lines = [];
-//   for (let index = 0; index < chars.length; index++) {
-//     if (maxWidth <= context.measureText(line + chars[index]).width) {
-//       lines.push(line);
-//       line = chars[index];
-//     } else {
-//       line += chars[index];
-//     }
-//   }
-//   lines.push(line);
-//   return lines;
-// };
+  functions.logger.info(img, {structuredData: true});
+
+  response.send(img);
+
+  // ファイル保存
+  fs.writeFile(fileName, img, function (err) {
+      console.log(err);
+  });
+});
+
+function splitByMeasureWidth(str, maxWidth, context) {
+  // サロゲートペアを考慮した文字分割
+  let chars = Array.from(str);
+  let line = '';
+  let lines = [];
+  for (let index = 0; index < chars.length; index++) {
+      if (maxWidth <= context.measureText(line + chars[index]).width) {
+          lines.push(line);
+          line = chars[index];
+      }
+      else {
+          line += chars[index];
+      }
+  }
+  lines.push(line);
+  return lines;
+}
